3f0de5749437f7a6543ca76c9287cd07b08d4d0a
hku2002 <hku2002@hanmail.net>
2023-11-19 00:23:13 +0900

    feat: Hello World! print job 추가

---
+++ first-practice/src/main/java/com/batch/tutorial/job/HelloWorldJobConfig.java
@@ -0,0 +1,41 @@
+package com.batch.tutorial.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+public class HelloWorldJobConfig {
+
+    @Bean
+    public Job helloWorldJob(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new JobBuilder("helloWorldJob", jobRepository)
+                .start(helloWorldStep(jobRepository, platformTransactionManager))
+                .build();
+    }
+
+    @Bean
+    public Step helloWorldStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("helloWorldStep", jobRepository)
+                .tasklet(helloWorldTasklet(), platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return (contribution, chunkContext) -> {
+            log.info(">>>>>> Hello World!");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+}
