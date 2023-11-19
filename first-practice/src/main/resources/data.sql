0dcbc58601282edc7fb04eade0eabf81aff541c3
hku2002 <hku2002@hanmail.net>
2023-11-19 15:42:38 +0900

    fix: data.sql 설정 수정

--- first-practice/src/main/resources/data.sql
+++ first-practice/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO orders (orderName, price, orderDateTime) VALUES ('첫번째 주문', 10000, '2023-11-15 10:00:01');
-INSERT INTO orders (orderName, price, orderDateTime) VALUES ('두번째 주문', 20000, '2023-11-16 10:00:02');
-INSERT INTO orders (orderName, price, orderDateTime) VALUES ('세번째 주문', 30000, '2023-11-17 10:00:03');
-INSERT INTO orders (orderName, price, orderDateTime) VALUES ('네번째 주문', 40000, '2023-11-18 10:00:04');
-INSERT INTO orders (orderName, price, orderDateTime) VALUES ('다섯번째 주문', 50000, '2023-11-19 10:00:05');
+INSERT INTO orders (order_name, price, order_date_time) VALUES ('첫번째 주문', 10000, '2023-11-15 10:00:01');
+INSERT INTO orders (order_name, price, order_date_time) VALUES ('두번째 주문', 20000, '2023-11-16 10:00:02');
+INSERT INTO orders (order_name, price, order_date_time) VALUES ('세번째 주문', 30000, '2023-11-17 10:00:03');
+INSERT INTO orders (order_name, price, order_date_time) VALUES ('네번째 주문', 40000, '2023-11-18 10:00:04');
+INSERT INTO orders (order_name, price, order_date_time) VALUES ('다섯번째 주문', 50000, '2023-11-19 10:00:05');
